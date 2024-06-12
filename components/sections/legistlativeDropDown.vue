8e3b901c5ce9aafb7c4bdd4046bc06ecbe6e56b2
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2024-06-12 11:26:00 +0100

    fix: typo and routing

--- components/sections/legistlativeDropDown.vue
+++ components/sections/legistlativeDropDown.vue
@@ -3,7 +3,7 @@
   <div class="w-full h-fit bg-primaryGreen">
     <div class="content mx-auto w-[85%] flex gap-8">
       <div class="text-area w-5/12 flex flex-col gap-4 py-24">
-        <h3 class="font-semibold gap-4" style="font-family: 'Inter', sans-serif">Legistlative Agenda</h3>
+        <h3 class="font-semibold gap-4" style="font-family: 'Inter', sans-serif">Legislative Agenda</h3>
         <h5 class="text-white font-medium" style="font-family: 'Inter', sans-serif">
           A prominent Nigerian politician, serving as the Deputy Speaker of the House of Representatives of Nigeria
           since 2023. Kalu represents the Bende
@@ -15,7 +15,7 @@
           style="
             background-image: url(https://res.cloudinary.com/damkhdi7d/image/upload/f_auto/v1717604743/benjamin_kalu/ben_native_geeiy8.png);
           "
-          @click="$router.push('/legistlative')"
+          @click="$router.push('/legislative')"
         >
           <p class="mx-auto w-11/12 mb-4 text-white flex items-center gap-1 relative z-10 cursor-pointer">
             Deputy Speaker's Agenda
@@ -27,6 +27,7 @@
           style="
             background-image: url(https://res.cloudinary.com/damkhdi7d/image/upload/f_auto/v1718144898/benjamin_kalu/gown_address_tdycbo.png);
           "
+          @click="$router.push('/bills')"
         >
           <p
             class="mx-auto w-11/12 mb-4 text-white flex items-center gap-1 relative z-10 cursor-pointer"
@@ -41,7 +42,7 @@
           style="
             background-image: url(https://res.cloudinary.com/damkhdi7d/image/upload/f_auto/v1718127396/benjamin_kalu/house_gravel_nccldk.png);
           "
-          @click="$router.push('/legistlative/review')"
+          @click="$router.push('/legislative/review')"
         >
           <p class="mx-auto w-11/12 mb-4 text-white flex items-center gap-1 relative z-10 cursor-pointer">
             Constitutional Review
