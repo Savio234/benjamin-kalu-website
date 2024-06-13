6a2c9aa6023a351d0001d4c7dc05267c7ac48d98
Okeibunor Emmanuel <okeibunoremma@gmail.com>
2024-06-13 21:02:34 +0100

    feat: expose api keys

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,8 +38,10 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      strapiURL: process.env.STRAPI_API,
-      strapiAPI: process.env.API_KEY,
+      strapiURL: process.env.STRAPI_API || 'https://benjamin-kalu-cms.onrender.com/api',
+      strapiAPI:
+        process.env.API_KEY ||
+        '920b6a15fcd2dc0e70cf9fbe96db28b11ad8f6bf3b45a39941b951f5d1d3f7eb348cfbd9944ded164c4b9c49dc9b5cee3f0f89eb29d2ce5dcc9137f3e66466f1b3be23ba69446ef44fd9ce83b6f8768cb4def4563c26dd615e9beb5ec19b94dbda236bb6cdd9a159259632bcf7cc5ef58b20ea62fff05c2860cbdd76c2fae383',
     },
   },
 });
