3d7b4f20612bdac55000bf10d7f80d09a72d5ea1
Favour Okeibunor <favourokeibunor@gmail.com>
2024-06-16 19:41:07 +0200

    remove sublogo whitespace

--- components/global/sublogo.vue
+++ components/global/sublogo.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="flex gap-2 items-center font-[600] sticky top-0 cursor-pointer bg-white p-2" @click="$router.push('/')">
     <NuxtImg src="images/logo.png" class="object-contain h-16 w-64" />
-
   </div>
 </template>
 
