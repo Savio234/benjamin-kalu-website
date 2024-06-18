d707868d120c59f171ddd438fae045ba5f81202f
Favour Okeibunor <favourokeibunor@gmail.com>
2024-06-18 17:25:56 +0200

    wip: mobile nav fix

--- components/global/NavBar.vue
+++ components/global/NavBar.vue
@@ -60,7 +60,7 @@
         @click="resetActivations"
       />
     </nav>
-    <div v-if="mobileNav" class="sm:hidden mobile-nav h-dvh w-full bg-black pt-8 fixed z-[1500] text-white">
+    <div v-if="mobileNav" class="mobile-nav h-[100vh] w-full bg-black pt-8 fixed z-[100] text-white">
       <!-- content -->
       <div class="flex flex-col h-screen gap-6 mx-auto w-[90%]">
         <NuxtLink to="/" class="text-white py-2 px-4 rounded-full" @click="closeMobileNav">Home</NuxtLink>
