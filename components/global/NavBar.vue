050f5e242d162bd7ad7c7c1d1606f285af35164a
Favour Okeibunor <favourokeibunor@gmail.com>
2024-06-18 16:38:44 +0200

    fix mobile navbar

--- components/global/NavBar.vue
+++ components/global/NavBar.vue
@@ -7,236 +7,158 @@
           <NuxtLink
             to="/"
             class="text-[#959897] py-2 px-4 rounded-full hover:text-black"
-            @mouseover="
-              activateAbout = false;
-              activateLegistlative = false;
-              activateBills = false;
-              activateProjects = false;
-            "
-            @click="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
+            @mouseover="resetActivations"
+            @click="resetActivations"
             >Home</NuxtLink
           >
           <NuxtLink
             to="/about"
             class="text-[#959897] py-2 px-4 rounded-full hover:text-black"
-            @mouseover="
-              activateAbout = true;
-              activateLegistlative = false;
-              activateBills = false;
-              activateProjects = false;
-            "
-            @click="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
+            @mouseover="activateSection('about')"
+            @click="resetActivations"
             >About</NuxtLink
           >
           <NuxtLink
             class="text-[#959897] py-2 px-4 rounded-full flex items-center gap-2 hover:text-black"
-            @mouseover="
-              activateLegistlative = true;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
-            @click="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
+            @mouseover="activateSection('legislative')"
+            @click="resetActivations"
             >Legislative Affairs
             <SvgIcon name="carat_down" width="1rem" height="1rem" />
           </NuxtLink>
-          <!-- <NuxtLink
-            to="/bills"
-            class="text-[#959897] py-2 px-4 rounded-full"
-            @mouseover="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
-            @click="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
-            >Bills & Motions</NuxtLink
-          > -->
           <NuxtLink
             class="text-[#959897] py-2 px-4 rounded-full flex items-center gap-2 hover:text-black"
-            @mouseover="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = true;
-            "
-            @click="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
+            @mouseover="activateSection('projects')"
+            @click="resetActivations"
             >Projects <SvgIcon name="carat_down" width="1rem" height="1rem"
           /></NuxtLink>
           <NuxtLink
             to="/blog"
             class="text-[#959897] py-2 px-4 rounded-full hover:text-black"
-            @mouseover="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
-            @click="
-              activateLegistlative = false;
-              activateAbout = false;
-              activateBills = false;
-              activateProjects = false;
-            "
+            @mouseover="resetActivations"
+            @click="resetActivations"
             >News & Update</NuxtLink
           >
         </div>
         <MainButton append-icon="sms" class="hidden lg:block" @click="$router.push('/contact')">Contact Us</MainButton>
 
         <!-- hamburger -->
-        <button class="block sm:hidden" @click="mobileNav = !mobileNav">
+        <button class="block sm:hidden" @click="toggleMobileNav">
           <svg-icon :name="mobileNav ? 'nav_close' : 'hamburger'" width="2rem" height="2rem" />
         </button>
       </div>
       <!-- dropdowns -->
-      <!-- <SectionsAboutDropDown
-        class="absolute top-[100%] shadow"
-        v-show="activateAbout"
-        @mouseleave="activateAbout = false"
-        @click="
-          activateLegistlative = false;
-          activateAbout = false;
-          activateBills = false;
-          activateProjects = false;
-        "
-      /> -->
       <SectionsLegistlativeDropDown
         class="absolute top-[100%] shadow"
-        v-show="activateLegistlative"
-        @mouseleave="activateLegistlative = false"
-        @click="
-          activateLegistlative = false;
-          activateAbout = false;
-          activateBills = false;
-          activateProjects = false;
-        "
+        v-show="activateLegislative"
+        @mouseleave="activateLegislative = false"
+        @click="resetActivations"
       />
-      <!-- <SectionsBillsDropDown
-        class="absolute top-[100%] shadow"
-        v-show="activateBills"
-        @mouseleave="activateBills = false"
-        @click="
-          activateLegistlative = false;
-          activateAbout = false;
-          activateBills = false;
-          activateProjects = false;
-        "
-      /> -->
       <SectionsProjectsDropDown
         class="absolute top-[100%] shadow"
         v-show="activateProjects"
         @mouseleave="activateProjects = false"
-        @click="
-          activateLegistlative = false;
-          activateAbout = false;
-          activateBills = false;
-          activateProjects = false;
-        "
+        @click="resetActivations"
       />
     </nav>
     <div v-if="mobileNav" class="sm:hidden mobile-nav h-dvh w-full bg-black pt-8 fixed z-[1500] text-white">
-    <!-- content -->
-    <div class="flex flex-col h-screen gap-6 mx-auto w-[90%]">
-      <NuxtLink to="/" class="text-white py-2 px-4 rounded-full" @click="mobileNav = false">Home</NuxtLink>
-      <NuxtLink to="/about" class="text-white py-2 px-4 rounded-full" @click="mobileNav = false">About</NuxtLink>
-      
-      <div>
-        <button @click="toggleSection('legislative')" class="w-full text-left text-white py-2 px-4 rounded-full focus:outline-none flex justify-between items-center">
-          Legislative Affairs
-          <font-awesome-icon :icon="['fas', 'chevron-down']" :class="{'rotate-180': openSection === 'legislative', 'rotate-0': openSection !== 'legislative'}" class="transform transition-transform duration-300"></font-awesome-icon>
-        </button>
-        <transition name="accordion">
-          <div v-show="openSection === 'legislative'" class="pl-4 overflow-hidden">
-            <NuxtLink to="/legislative" class="block text-white py-2 px-4 rounded-full" @click="mobileNav = false">Kalu's Missions</NuxtLink>
-            <NuxtLink to="/bills" class="block text-white py-2 px-4 rounded-full" @click="mobileNav = false">Bills & Motions</NuxtLink>
-            <NuxtLink to="/legislative/review" class="block text-white py-2 px-4 rounded-full" @click="mobileNav = false">Constitutional Review</NuxtLink>
-          </div>
-        </transition>
-      </div>
-      
-      <div>
-        <button @click="toggleSection('projects')" class="w-full text-left text-white py-2 px-4 rounded-full focus:outline-none flex justify-between items-center">
-          Projects
-          <font-awesome-icon :icon="['fas', 'chevron-down']" :class="{'rotate-180': openSection === 'projects', 'rotate-0': openSection !== 'projects'}" class="transform transition-transform duration-300"></font-awesome-icon>
-        </button>
-        <transition name="accordion">
-          <div v-show="openSection === 'projects'" class="pl-4 overflow-hidden">
-            <NuxtLink to="/projects" class="block text-white py-2 px-4 rounded-full" @click="mobileNav = false">Projects</NuxtLink>
-            <NuxtLink to="/projects/10" class="block text-white py-2 px-4 rounded-full" @click="mobileNav = false">PISEP</NuxtLink>
-          </div>
-        </transition>
-      </div>
+      <!-- content -->
+      <div class="flex flex-col h-screen gap-6 mx-auto w-[90%]">
+        <NuxtLink to="/" class="text-white py-2 px-4 rounded-full" @click="closeMobileNav">Home</NuxtLink>
+        <NuxtLink to="/about" class="text-white py-2 px-4 rounded-full" @click="closeMobileNav">About</NuxtLink>
+        
+        <div>
+          <button @click="toggleSection('legislative')" class="w-full text-left text-white py-2 px-4 rounded-full focus:outline-none flex justify-between items-center">
+            Legislative Affairs
+            <font-awesome-icon :icon="['fas', 'chevron-down']" :class="chevronClass('legislative')" class="transform transition-transform duration-300"></font-awesome-icon>
+          </button>
+          <transition name="accordion">
+            <div v-show="openSection === 'legislative'" class="pl-4 overflow-hidden">
+              <NuxtLink to="/legislative" class="block text-white py-2 px-4 rounded-full" @click="closeMobileNav">Kalu's Missions</NuxtLink>
+              <NuxtLink to="/bills" class="block text-white py-2 px-4 rounded-full" @click="closeMobileNav">Bills & Motions</NuxtLink>
+              <NuxtLink to="/legislative/review" class="block text-white py-2 px-4 rounded-full" @click="closeMobileNav">Constitutional Review</NuxtLink>
+            </div>
+          </transition>
+        </div>
+        
+        <div>
+          <button @click="toggleSection('projects')" class="w-full text-left text-white py-2 px-4 rounded-full focus:outline-none flex justify-between items-center">
+            Projects
+            <font-awesome-icon :icon="['fas', 'chevron-down']" :class="chevronClass('projects')" class="transform transition-transform duration-300"></font-awesome-icon>
+          </button>
+          <transition name="accordion">
+            <div v-show="openSection === 'projects'" class="pl-4 overflow-hidden">
+              <NuxtLink to="/projects" class="block text-white py-2 px-4 rounded-full" @click="closeMobileNav">Projects</NuxtLink>
+              <NuxtLink to="/projects/10" class="block text-white py-2 px-4 rounded-full" @click="closeMobileNav">PISEP</NuxtLink>
+            </div>
+          </transition>
+        </div>
 
-      <NuxtLink to="/blog" class="text-white py-2 px-4 rounded-full" @click="mobileNav = false">News & Update</NuxtLink>
+        <NuxtLink to="/blog" class="text-white py-2 px-4 rounded-full" @click="closeMobileNav">News & Update</NuxtLink>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const activateAbout = ref(false);
-const activateLegistlative = ref(false);
-const activateBills = ref(false);
+import { ref } from 'vue';
+
+const activateLegislative = ref(false);
 const activateProjects = ref(false);
 const mobileNav = ref(false);
-
 const openSection = ref<string | null>(null);
+
+const resetActivations = () => {
+  activateLegislative.value = false;
+  activateProjects.value = false;
+};
+
+const activateSection = (section: string) => {
+  resetActivations();
+  if (section === 'legislative') activateLegislative.value = true;
+  if (section === 'projects') activateProjects.value = true;
+};
+
+const toggleMobileNav = () => {
+  mobileNav.value = !mobileNav.value;
+};
+
+const closeMobileNav = () => {
+  mobileNav.value = false;
+};
 
 const toggleSection = (section: string) => {
   openSection.value = openSection.value === section ? null : section;
 };
 
+const chevronClass = (section: string) => {
+  return openSection.value === section ? 'rotate-180' : 'rotate-0';
+};
 </script>
 
 <style lang="scss" scoped>
-  nav {
-    font-family: 'Inter', sans-serif;
-  }
+nav {
+  font-family: 'Inter', sans-serif;
+}
 
-  .transition-max-height {
-    transition: max-height 0.5s ease-in-out;
-  }
+.transition-max-height {
+  transition: max-height 0.5s ease-in-out;
+}
 
-  .overflow-hidden {
-    overflow: hidden;
-  }
+.overflow-hidden {
+  overflow: hidden;
+}
 
-  .accordion-enter-active, .accordion-leave-active {
-    transition: max-height 0.5s ease-in-out;
-  }
-  .accordion-enter-from, .accordion-leave-to {
-    max-height: 0;
-  }
+.accordion-enter-active, .accordion-leave-active {
+  transition: max-height 0.5s ease-in-out;
+}
+.accordion-enter-from, .accordion-leave-to {
+  max-height: 0;
+}
 
-  .rotate-0 {
-    transform: rotate(0deg);
-  }
-  .rotate-180 {
-    transform: rotate(180deg);
-  }
+.rotate-0 {
+  transform: rotate(0deg);
+}
+.rotate-180 {
+  transform: rotate(180deg);
+}
 </style>
